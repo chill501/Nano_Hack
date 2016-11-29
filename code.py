dafec99f264d5ec7c2748a8669b15d2e8e8d8df9
chill501 <claire.hill@balliol.ox.ac.uk>
2016-11-29 11:21:01 +0000

    Update code.py

--- code.py
+++ code.py
@@ -2,9 +2,10 @@
 
 import sys
 inFile = sys.argv[1]
+outFile = '2d' + inFile
 
-with open(inFile,'r') as i:
-  for line in i:
-    if line.startswith(">") and ("Barcoding_2d" in line):
-       print(line)
-       print(next(i))
+with open(inFile,'r') as i, open(outFile, 'w') as o:
+	for line in i:
+		if line.startswith(">") and ("Barcoding_2d" in line):
+			o.write(line)
+			o.write(next(i))
