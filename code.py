82268b56180ca2c0f2b972e91dfe7172ee7c02fa
chill501 <claire.hill@balliol.ox.ac.uk>
2016-11-29 11:07:54 +0000

    Update code.py

--- code.py
+++ code.py
@@ -5,4 +5,6 @@ inFile = sys.argv[1]
 
 with open(inFile,'r') as i:
   for line in i:
-    if line startswith('>'
+    if line.startswith(">") and ("Barcoding_2d" in line):
+       print(line)
+       print(next(i))
